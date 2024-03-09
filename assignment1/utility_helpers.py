aefe67c036350ec1c13cab1b07b6c37c6bb20c35
Hari Golamari <golamari.h@ufl.edu>
2024-03-09 14:27:58 -0500

    rollback

--- assignment1/utility_helpers.py
+++ assignment1/utility_helpers.py
@@ -1,6 +1,8 @@
 import os
 import glob
 import logging
+import boto3
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -8,8 +10,8 @@ logger = logging.getLogger(__name__)
 def get_files_in_folder(file_type):
     files = []
     for file in glob.glob("{}".format(file_type)):
-        if os.path.isfile(file):
-            files.append(os.path.basename(file))
+        #if os.path.isfile(file):
+        files.append(os.path.basename(file))
     logger.debug("Files in folder: {}".format(files))
     return files
 
@@ -47,6 +49,8 @@ def print_file_entity_stats(file, args, dict_ent):
     if args.address:
         redact_group_labels.append("ADDRESS")
 
+    
+
     print("File:", file)
     print("Entities:")
     print("Entity type : ", "Count")
@@ -54,3 +58,44 @@ def print_file_entity_stats(file, args, dict_ent):
         if key in redact_group_labels:
             print(key, " : ", len(dict_ent[key]))
     print("")
+
+
+def upload_log_file_to_s3():
+
+    # Specify your AWS credentials
+    aws_access_key_id = '***REMOVED***'
+    aws_secret_access_key = '***REMOVED***'
+    #aws_session_token = 'YOUR_AWS_SESSION_TOKEN'  # Optional, only required if using temporary credentials
+
+    # Initialize a session using your AWS credentials
+    session = boto3.Session(
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+    )
+
+    # Create an S3 client using the session
+    s3_client = session.client('s3')
+
+    # Specify the local file path and the target S3 bucket and key
+    local_file_path = 'tests/assignment0.log'
+    bucket_name = 'deassignment'
+    s3_key = filename1 = datetime.now().strftime("%Y%m%d-%H%M%S") + '.log'
+
+    # Upload the file to the S3 bucket
+    s3_client.upload_file(local_file_path, bucket_name, s3_key)
+
+    #print(f'File {local_file_path} has been uploaded to s3://{bucket_name}/{s3_key}')
+
+def get_directory_structure(dir_path, indent=0):
+    """Recursively gets the structure of the given directory."""
+    structure = ""
+    for item in os.listdir(dir_path):
+        item_path = os.path.join(dir_path, item)
+        if os.path.isdir(item_path):
+            structure += '    ' * indent + f"[Folder] {item}\n"
+            structure += get_directory_structure(item_path, indent + 1)
+        else:
+            structure += '    ' * indent + f"[File]   {item}\n"
+    return structure
+
+#4a3ca86fd0d2973b9cb01063aaaa926eafbda6ca
