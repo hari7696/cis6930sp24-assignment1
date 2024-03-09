0fb7c911f539241df19021189adde070f631074b
Hari Golamari <golamari.h@ufl.edu>
2024-03-08 23:55:29 -0500

    basic version of working code: decently performance

--- assignment1/utility_helpers.py
+++ assignment1/utility_helpers.py
@@ -1,51 +1,56 @@
 import os
 import glob
 import logging
+
 logger = logging.getLogger(__name__)
+
 
 def get_files_in_folder(file_type):
     files = []
-    for file in glob.glob('text_files/{}'.format()):
-        if os.path.isfile(file):       
-            files.append(os.path.basename(file)) 
-    logger.debug('Files in folder: {}'.format(files))
+    for file in glob.glob("{}".format(file_type)):
+        if os.path.isfile(file):
+            files.append(os.path.basename(file))
+    logger.debug("Files in folder: {}".format(files))
     return files
+
 
 def redact(text, dict_ent, args):
 
-    #redact_group_labels = [item  for item in [args.names, args.dates, args.phones, args.address] if item]
+    # redact_group_labels = [item  for item in [args.names, args.dates, args.phones, args.address] if item]
     redact_group_labels = []
     if args.names:
-        redact_group_labels.append('PERSON')
+        redact_group_labels.append("PERSON")
     if args.dates:
-        redact_group_labels.append('DATE')
+        redact_group_labels.append("DATE")
     if args.phones:
-        redact_group_labels.append('PHONE')
+        redact_group_labels.append("PHONE")
     if args.address:
-        redact_group_labels.append('ADDRESS')
+        redact_group_labels.append("ADDRESS")
 
     for key in dict_ent:
-        if key in args:
-            start, end = dict_ent[key]
-            text = text[:start] + '\u2588' * (end - start) + text[end:]
+        if key in redact_group_labels:
+            for item in dict_ent[key]:
+                start, end = item[0], item[1]
+                text = text[:start] + "\u2588" * (end - start) + text[end:]
     return text
+
 
 def print_file_entity_stats(file, args, dict_ent):
 
     redact_group_labels = []
     if args.names:
-        redact_group_labels.append('PERSON')
+        redact_group_labels.append("PERSON")
     if args.dates:
-        redact_group_labels.append('DATE')
+        redact_group_labels.append("DATE")
     if args.phones:
-        redact_group_labels.append('PHONE')
+        redact_group_labels.append("PHONE")
     if args.address:
-        redact_group_labels.append('ADDRESS')
+        redact_group_labels.append("ADDRESS")
 
-    print('File:', file)
-    print('Entities:')
-    print('Entity type : ', 'Count')
+    print("File:", file)
+    print("Entities:")
+    print("Entity type : ", "Count")
     for key in dict_ent:
         if key in redact_group_labels:
-            print(key, ' : ',len(dict_ent[key]))
-    print('')
+            print(key, " : ", len(dict_ent[key]))
+    print("")
---
+++ tests/test_utilities.py
@@ -0,0 +1,33 @@
+import pytest
+from assignment1.utility_helpers import redact, print_file_entity_stats, get_files_in_folder
+import argparse 
+import warnings
+warnings.filterwarnings("ignore")
+def test_redact():
+    text = "John Doe was born on 12/12/1990 in New York. His phone number is 123-456-7890 and his email is"
+    dict_ent = {'PERSON': [(0, 8, 'John Doe')], 'DATE': [(21, 31, '12/12/1990')]}
+
+    parser = argparse.ArgumentParser(description="Censor text files.")
+
+    parser.add_argument("--names", action="store_false", help="Censor names")
+    parser.add_argument("--dates", action="store_false", help="Censor dates")
+    parser.add_argument("--phones", action="store_true", help="Censor phone numbers")
+    parser.add_argument("--address", action="store_true", help="Censor addresses")
+    args = parser.parse_args()
+
+
+    redacted_text = redact(text, dict_ent,args)
+
+    print("sadkjasjd" ,redacted_text)
+    assert redacted_text == "████████ was born on ██████████ in New York. His phone number is 123-456-7890 and his email is"
+
+def test_print_file_entity_stats():
+    dict_ent = {'PERSON': [(0, 8, 'John Doe')], 'DATE': [(23, 33, '12/12/1990')]}
+    parser = argparse.ArgumentParser(description="Censor text files.")
+    parser.add_argument("--names", action="store_false", help="Censor names")
+    parser.add_argument("--dates", action="store_false", help="Censor dates")
+    parser.add_argument("--phones", action="store_true", help="Censor phone numbers")
+    parser.add_argument("--address", action="store_true", help="Censor addresses")
+    args = parser.parse_args()
+    print_file_entity_stats('test.txt', args, dict_ent)
+    assert True
