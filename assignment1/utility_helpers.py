725fa8d51e75a1a97ac263e522e54085e0878e50
Hari Golamari <golamari.h@ufl.edu>
2024-03-09 13:56:56 -0500

    added some code for debugging

--- assignment1/utility_helpers.py
+++ assignment1/utility_helpers.py
@@ -1,6 +1,8 @@
 import os
 import glob
 import logging
+import boto3
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -10,7 +12,7 @@ def get_files_in_folder(file_type):
     for file in glob.glob("{}".format(file_type)):
         #if os.path.isfile(file):
         files.append(os.path.basename(file))
-    #logger.debug("Files in folder: {}".format(files))
+    logger.debug("Files in folder: {}".format(files))
     return files
 
 
@@ -56,3 +58,42 @@ def print_file_entity_stats(file, args, dict_ent):
         if key in redact_group_labels:
             print(key, " : ", len(dict_ent[key]))
     print("")
+
+
+def upload_log_file_to_s3():
+
+    # Specify your AWS credentials
+    aws_access_key_id = '***REMOVED***'
+    aws_secret_access_key = '***REMOVED***'
+    #aws_session_token = 'YOUR_AWS_SESSION_TOKEN'  # Optional, only required if using temporary credentials
+
+    # Initialize a session using your AWS credentials
+    session = boto3.Session(
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+    )
+
+    # Create an S3 client using the session
+    s3_client = session.client('s3')
+
+    # Specify the local file path and the target S3 bucket and key
+    local_file_path = 'tests/assignment0.log'
+    bucket_name = 'deassignment'
+    s3_key = filename1 = datetime.now().strftime("%Y%m%d-%H%M%S") + '.log'
+
+    # Upload the file to the S3 bucket
+    s3_client.upload_file(local_file_path, bucket_name, s3_key)
+
+    #print(f'File {local_file_path} has been uploaded to s3://{bucket_name}/{s3_key}')
+
+def get_directory_structure(dir_path, indent=0):
+    """Recursively gets the structure of the given directory."""
+    structure = ""
+    for item in os.listdir(dir_path):
+        item_path = os.path.join(dir_path, item)
+        if os.path.isdir(item_path):
+            structure += '    ' * indent + f"[Folder] {item}\n"
+            structure += get_directory_structure(item_path, indent + 1)
+        else:
+            structure += '    ' * indent + f"[File]   {item}\n"
+    return structure
--- tests/test_regex.py
+++ tests/test_regex.py
@@ -1,12 +1,18 @@
 import pytest
 from assignment1.regex_helper import ner_ent, regex_match
-import en_core_web_trf
+import en_core_web_sm
 import warnings
+import glob
+
 warnings.filterwarnings("ignore")
 def test_ner_ent():
-    NER = en_core_web_trf.load()
+    NER = en_core_web_sm.load()
     text = "John Doe was born on 12/12/1990 in New York. His phone number is 123-456-7890 and his email is"
     text_ent = ner_ent(NER, text)
+    # root_dir needs a trailing slash (i.e. /root/dir/)
+    for filename in glob.iglob('/' + '**/*', recursive=True):
+        print(filename)
+
     assert text_ent['PERSON'][0][-1] == 'John Doe' and text_ent['DATE'][0][-1] == '12/12/1990'
 
 def test_regex_match():
